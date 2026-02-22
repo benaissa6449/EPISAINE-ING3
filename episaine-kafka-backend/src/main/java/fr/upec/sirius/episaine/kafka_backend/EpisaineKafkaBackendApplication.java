2164d4fb52ccdd02316b758eeab8a62b53296171
Ismail Benaissa <ismailbenaissa85@gmail.com>
2026-02-22 17:07:55 +0100

    chore(backend): configure startup CORS defaults

--- episaine-kafka-backend/src/main/java/fr/upec/sirius/episaine/kafka_backend/EpisaineKafkaBackendApplication.java
+++ episaine-kafka-backend/src/main/java/fr/upec/sirius/episaine/kafka_backend/EpisaineKafkaBackendApplication.java
@@ -2,8 +2,10 @@ package fr.upec.sirius.episaine.kafka_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class EpisaineKafkaBackendApplication {
 
     public static void main(String[] args) {
